16ef7d49c1f2ad06806f9a3c122a386fdd99a03f
joodh999 <joodhm95@gmail.com>
2020-08-23 17:54:03 +0500

    production

---
+++ build/precache-manifest.cdcb341f436c9475e07811f72548fd03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4770f1967a326e02168bee04d9fa5002",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45d7092003c6c8125ba3",
+    "url": "/static/css/main.6de396ad.chunk.css"
+  },
+  {
+    "revision": "12d513f06ef64e7260f9",
+    "url": "/static/js/2.471cd1fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.471cd1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d7092003c6c8125ba3",
+    "url": "/static/js/main.ee352ff8.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
